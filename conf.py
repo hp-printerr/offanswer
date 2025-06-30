f11fbdc1338d0683e0f2e4ac25eed445e4e4ec1e
hp-printerr <106080415+hp-printerr@users.noreply.github.com>
2025-06-30 19:44:34 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -16,7 +16,7 @@ import sys
 
 # -- Project information -----------------------------------------------------
 
-project = 'HP Smart'
+project = 'Office Setup'
 
 
 # The full version, including alpha/beta/rc tags
